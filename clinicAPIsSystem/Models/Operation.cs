c6de5f598fefe840881c5401a0df071a8b52c2d9
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-07-25 18:06:41 +0300

    add forgot operation model , for paying

---
+++ clinicAPIsSystem/Models/Operation.cs
@@ -0,0 +1,18 @@
+﻿using clinicAPIsSystem.Models.User;
+using clinicAPIsSystem.Models.User.NonMedicalStaff;
+
+namespace clinicAPIsSystem.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; } 
+        public Accountant Accountant { get; set; } = null!; 
+        public int AccountantId { get; set; } 
+        public Patient Patient { get; set; } = null!; 
+        public int PatientId { get; set; } 
+        public Appointment Appointment { get; set; } = null!; 
+        public int AppointmentId { get; set; } 
+        public decimal Amount { get; set; } 
+
+    }
+}
