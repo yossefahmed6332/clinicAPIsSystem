a24c7037bc9e10e5e745f536127aa8612478012a
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-07-25 19:04:20 +0300

    create database configuration for Medical staff

---
+++ clinicAPIsSystem/Data/Configuration/MedicalStaffConfigurations/NurseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using clinicAPIsSystem.Models.User.MedicalStaff;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+namespace clinicAPIsSystem.Data.Configuration.MedicalStaffConfiguration
+{
+    public class NurseConfiguration: IEntityTypeConfiguration<Nurse>
+    {
+        public void Configure(EntityTypeBuilder<Nurse> builder)
+        {
+
+        }
+    }
+}
