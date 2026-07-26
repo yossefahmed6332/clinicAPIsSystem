d20aba3262cc5366657f7036bca0e4850cb82bff
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-07-26 12:23:11 +0300

    finish medical staff data configuration

---
+++ clinicAPIsSystem/Data/ClinicConfiguration/UserConfiguration/MedicalStaffConfiguration/MedicalStaffConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using clinicAPIsSystem.Models.User.MedicalStaff;
+namespace clinicAPIsSystem.Data.ClinicConfiguration.UserConfiguration.MedicalStaffConfiguration
+{
+    public class MedicalStaffConfiguration: IEntityTypeConfiguration<MedicalStaff>
+    {
+        public void Configure(EntityTypeBuilder<MedicalStaff> builder)
+        {
+            builder.Property(ms => ms.YearsOfExperience)
+                .IsRequired(); 
+            builder.Property(ms => ms.LicenseNumber)
+                .IsRequired()
+                .HasMaxLength(50); 
+            builder.Property(ms => ms.QualificationId)
+                .IsRequired(); 
+
+            //set relations 
+            builder.HasOne(ms => ms.Qualification)
+                .WithMany(q => q.MedicalStaffs)
+                .HasForeignKey(ms => ms.QualificationId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ clinicAPIsSystem/Data/ClinicConfiguration/UserConfiguration/MedicalStaffConfiguration/NurseConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using clinicAPIsSystem.Models.User.MedicalStaff;
+
+namespace clinicAPIsSystem.Data.ClinicConfiguration.UserConfiguration.MedicalStaffConfiguration
+{
+    public class NurseConfiguration : IEntityTypeConfiguration<Nurse>
+    {
+        public void Configure(EntityTypeBuilder<Nurse> builder)
+        {
+            // Additional configuration for Nurse entity can be added here if needed>
+            {
+            }
+        }
+    }
+}
