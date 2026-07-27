73eb6f4ca0ec0c56ab8616dc27eb6b8dc9485aa8
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-07-28 00:13:08 +0300

    add forgotten controller : PatientController

---
+++ clinicAPIsSystem/Controllers/UserController/PatientController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using clinicAPIsSystem.ClinicDTOs.UserDTOs.PatientDTOs;
+using clinicAPIsSystem.Interfaces.IUserService;
+using Microsoft.AspNetCore.Authorization;
+using clinicAPIsSystem.Models;
+
+namespace clinicAPIsSystem.Controllers.UserController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+
+        private readonly IPatientService _patientService;
+        public PatientController(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
+        [Authorize(Roles = nameof(Roles.Admin))]
+        [HttpPost("CreatePatient")]
+        public async Task<IActionResult> CreatePatientAsync([FromBody] CreatePatientDto dto)
+        {
+            await _patientService.CreatePatientAsync(dto);
+            return Ok();
+        }
+
+
+    }
+}
