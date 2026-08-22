dd929860cf4a63388793a7e04e406648d4c66a6d
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 02:03:21 +0300

    Belongs to the last committee.

--- clinicAPIsSystem/Models/User/Patient.cs
+++ clinicAPIsSystem/Models/User/Patient.cs
@@ -3,9 +3,6 @@
     public class Patient: ApplicationUser
     {
         public ICollection<Appointment> Appointments { get;  set; } = new HashSet<Appointment>();
-        public ICollection<Prescription> Prescriptions { get;  set; } = new HashSet<Prescription>();
-        public ICollection <VitalSigns> VitalSigns { get;  set; } = new HashSet<VitalSigns>();
-        public ICollection<ExaminationResult> ExaminationResults { get;  set; } = new HashSet<ExaminationResult>(); 
         public MedicalRecord MedicalRecord {  get;  set; } = new MedicalRecord();
         public int MedicalRecordId { get; set; } 
 
