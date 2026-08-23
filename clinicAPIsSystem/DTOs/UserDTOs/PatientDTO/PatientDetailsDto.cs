6de6aaf3743593865842311468e07aa0d00566b0
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 07:05:02 +0300

    finish PatientDTO

---
+++ clinicAPIsSystem/DTOs/UserDTOs/PatientDTO/PatientDetailsDto.cs
@@ -0,0 +1,21 @@
+﻿using clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO;
+
+namespace clinicAPIsSystem.DTOs.UserDTOs.PatientDTO
+{
+    public class PatientDetailsDto:ApplicationUserDto
+    {
+        public ICollection<AppointmentDto> Appointments { get; private set; }
+        public ICollection<PaymantOperationDto> PaymaentOperation { get; private set; }
+        public PatientDetailsDto(int id, string userName, string email, string firstName, string lastName, string phoneNumber, ICollection<AppointmentDto> appointments, ICollection<PaymantOperationDto> paymentOperations)
+            : base(id, userName, email, firstName, lastName, phoneNumber)
+        {
+            Appointments = appointments;
+            PaymaentOperation = paymentOperations;
+        }
+
+
+
+
+
+    }
+}
--- clinicAPIsSystem/DTOs/UserDTOs/PatientDTO/PatientDto.cs
+++ clinicAPIsSystem/DTOs/UserDTOs/PatientDTO/PatientDto.cs
@@ -1,11 +1,16 @@
 ﻿using clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO;
+using System.ComponentModel.DataAnnotations;
 namespace clinicAPIsSystem.DTOs.UserDTOs.PatientDTO
 {
     public class PatientDto: ApplicationUserDto
     {
-        public PatientDto(int id, string userName, string email, string firstName, string lastName, string phoneNumber)
+        [Required,Range(1,int.MaxValue)]
+        public int MedicalRecordId { get; private set; }
+
+        public PatientDto(int id, string userName, string email, string firstName, string lastName, string phoneNumber, int medicalRecordId)
             : base(id, userName, email, firstName, lastName, phoneNumber)
         {
+            MedicalRecordId = medicalRecordId;
         }
     }
     
