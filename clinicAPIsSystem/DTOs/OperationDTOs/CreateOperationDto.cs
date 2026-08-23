30a50ccdca06b0742d324dca38fadaf5f4f447bc
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 08:43:15 +0300

    finish OperationDTOs

---
+++ clinicAPIsSystem/DTOs/OperationDTOs/CreateOperationDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.OperationDTOs
+{
+    public class CreateOperationDto
+    {
+        [Required, Range(0.01, double.MaxValue)]
+        public decimal Amount { get; private set; }
+        [Required]
+        public DateTime Date { get; private set; }
+        [Required, EnumDataType(typeof(OperationType))]
+        public OperationType OperationType { get; private set; }
+        [Required, EnumDataType(typeof(OperationStatus))]
+        public OperationStatus OperationStatus { get; private set; }
+        [Range(1, int.MaxValue)]
+        public int? PatientId { get; private set; }
+        [Range(1, int.MaxValue)]
+        public int? AccountantId { get; private set; }
+        [Required, EnumDataType(typeof(PaymentMethod))]
+        public PaymentMethod PaymentMethod { get; private set; }
+
+        public CreateOperationDto(decimal amount, DateTime date, OperationType operationType, OperationStatus operationStatus, int? patientId, int? accountantId, PaymentMethod paymentMethod)
+        {
+            Amount = amount;
+            Date = date;
+            OperationType = operationType;
+            OperationStatus = operationStatus;
+            PatientId = patientId;
+            AccountantId = accountantId;
+            PaymentMethod = paymentMethod;
+        }
+    }
+}
---
+++ clinicAPIsSystem/DTOs/OperationDTOs/OperationDto.cs
@@ -0,0 +1,26 @@
+﻿namespace clinicAPIsSystem.DTOs.OperationDTOs
+{
+    public class OperationDto
+    {
+        public int Id { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateTime Date { get; private set; }
+        public OperationType OperationType { get; private set; }
+        public OperationStatus OperationStatus { get; private set; }
+        public int? PatientId { get; private set; }
+        public int? AccountantId { get; private set; }
+        public PaymentMethod PaymentMethod { get; private set; }
+
+        public OperationDto(int id, decimal amount, DateTime date, OperationType operationType, OperationStatus operationStatus, int? patientId, int? accountantId, PaymentMethod paymentMethod)
+        {
+            Id = id;
+            Amount = amount;
+            Date = date;
+            OperationType = operationType;
+            OperationStatus = operationStatus;
+            PatientId = patientId;
+            AccountantId = accountantId;
+            PaymentMethod = paymentMethod;
+        }
+    }
+}
