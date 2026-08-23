73ea848cad158fa241d1e3c7e2fe495163fb6a97
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 08:12:49 +0300

    finish VitalSignsDTOs

---
+++ clinicAPIsSystem/DTOs/VitalSignsDTOs/UpdateVitalSignsDto.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.VitalSignsDTOs
+{
+    public class UpdateExaminationResultDto
+    {
+
+        [Required, Range(0, 300)]
+        public int BloodPressureSystolic { get; private set; }
+        [Required, Range(0, 300)]
+        public int BloodPressureDiastolic { get; private set; }
+        [Required, Range(0, 300)]
+        public int HeartRate { get; private set; }
+        [Required, Range(0, 45)]
+        public decimal Temperature { get; private set; }
+        [Required, Range(0, 100)]
+        public decimal OxygenSaturation { get; private set; }
+        [Required]
+        public DateTime RecordedAt { get; private set; }
+        [Required, Range(1, int.MaxValue)]
+        public int NurseId { get; private set; }
+        [Required, Range(1, int.MaxValue)]
+        public int PatientId { get; private set; }
+
+        public UpdateExaminationResultDto(int bloodPressureSystolic, int bloodPressureDiastolic, int heartRate, decimal temperature, decimal oxygenSaturation, DateTime recordedAt, int nurseId, int patientId)
+        {
+            BloodPressureSystolic = bloodPressureSystolic;
+            BloodPressureDiastolic = bloodPressureDiastolic;
+            HeartRate = heartRate;
+            Temperature = temperature;
+            OxygenSaturation = oxygenSaturation;
+            RecordedAt = recordedAt;
+            NurseId = nurseId;
+            PatientId = patientId;
+        }
+
+
+    }
+}
---
+++ clinicAPIsSystem/DTOs/VitalSignsDTOs/VitalSignsDto.cs
@@ -0,0 +1,28 @@
+﻿namespace clinicAPIsSystem.DTOs.VitalSignsDTOs
+{
+    public class VitalSignsDto
+    {
+        public int Id { get; private set; }
+        public int BloodPressureSystolic { get; private set; }
+        public int BloodPressureDiastolic { get; private set; }
+        public int HeartRate { get; private set; }
+        public decimal Temperature { get; private set; }
+        public decimal OxygenSaturation { get; private set; }
+        public DateTime RecordedAt { get; private set; }
+        public int NurseId { get; private set; }
+        public int PatientId { get; private set; }
+
+        public VitalSignsDto(int id, int bloodPressureSystolic, int bloodPressureDiastolic, int heartRate, decimal temperature, decimal oxygenSaturation, DateTime recordedAt, int nurseId, int patientId)
+        {
+            Id = id;
+            BloodPressureSystolic = bloodPressureSystolic;
+            BloodPressureDiastolic = bloodPressureDiastolic;
+            HeartRate = heartRate;
+            Temperature = temperature;
+            OxygenSaturation = oxygenSaturation;
+            RecordedAt = recordedAt;
+            NurseId = nurseId;
+            PatientId = patientId;
+        }
+    }
+}
