d6b2c75f2359f5bd816bf7d9469b2c42e143fd2e
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 07:08:34 +0300

    finish GraduatedDTO

---
+++ clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/GraduatedDTO/GraduatedDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO.Employees.GraduatedDTO
+{
+    public class GraduatedDto:EmployeeDto
+    {
+        public string Degree { get; protected set; }
+        public string University { get; protected set; }
+        public string YearsOfExperience { get; protected set; }
+        public int GraduationYear { get; protected set; }
+        public string LicenseNumber { get; protected set; }
+
+        public GraduatedDto(int id, string userName, string email, string firstName, string lastName, string phoneNumber, decimal salaryPerHour, int hoursWorked, TimeOnly shiftStart, TimeOnly shiftEnd,string degree,string university,string yearsOfExperience,int graduationYear,string licenseNumber)
+            : base(id, userName, email, firstName, lastName, phoneNumber, salaryPerHour, hoursWorked, shiftStart, shiftEnd)
+        {
+            Degree = degree;
+            University = university;
+            YearsOfExperience = yearsOfExperience;
+            GraduationYear = graduationYear;
+            LicenseNumber = licenseNumber;
+        }
+    }
+}
---
+++ clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/GraduatedDTO/UpdateGraduatedDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO.Employees.GraduatedDTO
+{
+    public class UpdateGraduatedDto : UpdateEmployeeDto
+    {
+        [Required, MaxLength(100)]
+        public string Degree { get; protected set; }
+        [Required, MaxLength(100)]
+        public string University { get; protected set; }
+        [Required, MaxLength(100)]
+        public string YearsOfExperience { get; protected set; }
+        [Required, Range(1900, 2600)]
+        public int GraduationYear { get; protected set; }
+        [Required, MaxLength(100)]
+        public string LicenseNumber { get; protected set; }
+
+        public UpdateGraduatedDto(string firstName, string lastName, string email, string userName, string phoneNumber, decimal salaryPerHour, int hoursWorked, TimeOnly shiftStart, TimeOnly shiftEnd, string degree, string university, string yeatsOfExperience, int graduationYear, string licenseNumber)
+            : base(firstName, lastName, email, userName, phoneNumber, salaryPerHour, hoursWorked, shiftStart, shiftEnd)
+        {
+            Degree = degree;
+            University = university;
+            YearsOfExperience = yeatsOfExperience;
+            GraduationYear = graduationYear;
+            LicenseNumber = licenseNumber;
+        }
+
+    }
+}
