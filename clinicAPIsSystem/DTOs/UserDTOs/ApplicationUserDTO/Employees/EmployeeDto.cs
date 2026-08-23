cd76bd4288070f2f1fd61add19f568c92132c22d
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 07:08:51 +0300

    finish EmployeeDTO

--- clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/EmployeeDto.cs
+++ clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/EmployeeDto.cs
@@ -1,23 +1,12 @@
 ﻿namespace clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO.Employees
 {
-    public class EmployeeDto : ApplicationUserDTO
+    public class EmployeeDto: ApplicationUserDto
     {
-        public decimal SalaryPerHour { get; private set; }
-        public int HoursWorked { get; private set; }
-        public TimeOnly ShiftStart { get; private set; }
-        public TimeOnly ShiftEnd { get; private set; }
-
-        public EmployeeDto(
-            int id,
-            string userName,
-            string email,
-            string firstName,
-            string lastName,
-            string phoneNumber,
-            decimal salaryPerHour,
-            int hoursWorked,
-            TimeOnly shiftStart,
-            TimeOnly shiftEnd)
+        public decimal SalaryPerHour { get; protected set; }
+        public int HoursWorked { get; protected set; }
+        public TimeOnly ShiftStart { get; protected set; }
+        public TimeOnly ShiftEnd { get; protected set; }
+        public EmployeeDto(int id, string userName, string email, string firstName, string lastName, string phoneNumber, decimal salaryPerHour, int hoursWorked, TimeOnly shiftStart, TimeOnly shiftEnd)
             : base(id, userName, email, firstName, lastName, phoneNumber)
         {
             SalaryPerHour = salaryPerHour;
@@ -25,5 +14,6 @@
             ShiftStart = shiftStart;
             ShiftEnd = shiftEnd;
         }
+    {
     }
 }
--- clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/UpdateEmployeeDto.cs
+++ clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/UpdateEmployeeDto.cs
@@ -5,13 +5,13 @@ namespace clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO.Employees
     public class UpdateEmployeeDto : UpdateApplicationUserDto
     {
         [Required, Range(typeof(decimal), "0", "79228162514264337593543950335")]
-        public decimal SalaryPerHour { get; private set; }
+        public decimal SalaryPerHour { get; protected set; }
         [Required, Range(0, int.MaxValue)]
-        public int HoursWorked { get; private set; }
+        public int HoursWorked { get; protected set; }
         [Required]
-        public TimeOnly ShiftStart { get; private set; }
+        public TimeOnly ShiftStart { get; protected set; }
         [Required]
-        public TimeOnly ShiftEnd { get; private set; }
+        public TimeOnly ShiftEnd { get; protected set; }
 
 
         public UpdateEmployeeDto(string firstName, string lastName, string email, string userName, string phoneNumber, decimal salaryPerHour, int hoursWorked, TimeOnly shiftStart, TimeOnly shiftEnd)
