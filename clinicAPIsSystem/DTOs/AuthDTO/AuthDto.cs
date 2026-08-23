808975494c69ae8bdf91428e4ac5f20950f5533b
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 07:38:23 +0300

    finish AuthDto.cs

---
+++ clinicAPIsSystem/DTOs/AuthDTO/AuthDto.cs
@@ -0,0 +1,13 @@
+﻿namespace clinicAPIsSystem.DTOs.AuthDTO
+{
+    public class AuthDto
+    {
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public AuthDto(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
