eb80771409a16c5555001c241bb7bfe51b192105
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 08:01:59 +0300

    finish ExaminationResultDTO

---
+++ clinicAPIsSystem/DTOs/ExaminationResultDTOs/ExaminationResultDto.cs
@@ -0,0 +1,30 @@
+﻿namespace clinicAPIsSystem.DTOs.ExaminationResultDTOs
+{
+    public class ExaminationResultDto
+    {
+        public int Id { get; private set; }
+        public string TestType { get; private set; }
+        public string ResultValue { get; private set; }
+        public string Unit { get; private set; }
+        public string NormalRange { get; private set; }
+        public string Note { get; private set; }
+        public DateTime RecordedAt { get; private set; } 
+        public int PatientId { get; private set; }
+        public int NurseId { get; private set; }
+
+        public ExaminationResultDto(int id, string testType, string resultValue, string unit, string normalRange, string note, DateTime recordedAt, int patientId, int nurseId)
+        {
+            Id = id;
+            TestType = testType;
+            ResultValue = resultValue;
+            Unit = unit;
+            NormalRange = normalRange;
+            Note = note;
+            RecordedAt = recordedAt;
+            PatientId = patientId;
+            NurseId = nurseId;
+        }
+
+    }
+    }
+
---
+++ clinicAPIsSystem/DTOs/ExaminationResultDTOs/UpdateExaminationResultDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.ExaminationResultDTOs
+{
+    public class UpdateExaminationResultDto
+    {
+        [Required, MaxLength(500)]
+        public string TestType { get; private set; }
+        [Required, MaxLength(50)]
+        public string ResultValue { get; private set; }
+        [Required, MaxLength(50)]
+        public string Unit { get; private set; }
+        [Required, MaxLength(100)]
+        public string NormalRange { get; private set; }
+        [Required, MaxLength(1000)]
+        public string Note { get; private set; }
+        [Required]
+        public DateTime RecordedAt { get; private set; }
+        [Required, Range(1, int.MaxValue)]
+        public int PatientId { get; private set; }
+        [Required, Range(1, int.MaxValue)]
+        public int NurseId { get; private set; }
+
+
+        public UpdateExaminationResultDto(string testType, string resultValue, string unit, string normalRange, string note, DateTime recordedAt, int patientId, int nurseId)
+        {
+            TestType = testType;
+            ResultValue = resultValue;
+            Unit = unit;
+            NormalRange = normalRange;
+            Note = note;
+            RecordedAt = recordedAt;
+            PatientId = patientId;
+            NurseId = nurseId;
+        }
+    }
+}
