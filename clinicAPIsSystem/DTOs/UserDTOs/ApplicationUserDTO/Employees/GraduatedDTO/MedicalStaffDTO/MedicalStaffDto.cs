1ac21b3419d296b587a90b448599b76b966161ba
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 07:08:15 +0300

    finish MedicalStaffDTO

---
+++ clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/GraduatedDTO/MedicalStaffDTO/MedicalStaffDto.cs
@@ -0,0 +1,14 @@
+﻿namespace clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO.Employees.GraduatedDTO.MedicalStaffDTO
+{
+    public class MedicalStaffDto: GraduatedDto
+    {
+        public string Specialization { get; protected set; }
+        public MedicalStaffDto(int id, string userName, string email, string firstName, string lastName, string phoneNumber, decimal salaryPerHour, int hoursWorked, TimeOnly shiftStart, TimeOnly shiftEnd, string degree, string university, string yearsOfExperience, int graduationYear, string licenseNumber, string specialization)
+            : base(id, userName, email, firstName, lastName, phoneNumber, salaryPerHour, hoursWorked, shiftStart, shiftEnd, degree, university, yearsOfExperience, graduationYear, licenseNumber)
+        {
+            Specialization = specialization;
+        }
+    }
+    
+    
+}
