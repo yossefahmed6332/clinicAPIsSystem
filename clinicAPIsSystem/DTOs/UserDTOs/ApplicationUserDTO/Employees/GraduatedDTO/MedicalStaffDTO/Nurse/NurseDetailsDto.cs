0735e21ca65291eff684cca024249a2ece0d56ee
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 07:18:16 +0300

    finish NurseDTO

---
+++ clinicAPIsSystem/DTOs/UserDTOs/ApplicationUserDTO/Employees/GraduatedDTO/MedicalStaffDTO/Nurse/NurseDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿
+namespace clinicAPIsSystem.DTOs.UserDTOs.ApplicationUserDTO.Employees.GraduatedDTO.MedicalStaffDTO.Nurse
+{
+    public class NurseDetailsDto:MedicalStaffDto
+    {
+        public ICollection<VitalSignsDto> VitalSignsRecordedDtos {  get; private set; }
+        public ICollection<ExaminationResultDto> ExaminationResultDtos { get; private set; }
+
+        public NurseDetailsDto(int id,string userName, string email, string firstName, string lastName, string phoneNumber, decimal salaryPerHour, int hoursWorked, TimeOnly shiftStart, TimeOnly shiftEnd, string degree, string university, string yearsOfExperience, int graduationYear, string licenseNumber, string specialization,ICollection<VitalSignsDto> vitalSignsRecorded,ICollection<ExaminationResultDto> examinationResults)
+    : base(id,userName, email, firstName, lastName, phoneNumber, salaryPerHour, hoursWorked, shiftStart, shiftEnd, degree, university, yearsOfExperience, graduationYear, licenseNumber, specialization)
+        {
+            VitalSignsRecordedDtos= vitalSignsRecorded;
+            ExaminationResultDtos= examinationResults;
+        }
+
+    }
+}
