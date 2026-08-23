b24218051600aa4dfd1449c82abc1906a8b8e81a
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-23 08:19:10 +0300

    finish PrescriptionDTOs

---
+++ clinicAPIsSystem/DTOs/PrescriptionDTOs/CreatePrescriptionDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.PrescriptionDTOs
+{
+    public class CreatePrescriptionDto
+    {
+        [Required,MaxLength(100)]
+        public string MedicalName { get; private set; }
+        [Required,MaxLength(100)]
+        public string Dosage { get; private set; }
+        [Required,MaxLength(100)]
+        public string Frequency { get; private set; }
+        [Required,MaxLength(100)]
+        public string Duration { get; private set; }
+        [Required,MaxLength(500)]
+        public string Instructions { get; private set; }
+        [Required,MaxLength(100)]
+        public string Diagnosis { get; private set; }
+        public int DoctorId { get; private set; }
+        public int PatientId { get; private set; }
+
+        public CreatePrescriptionDto(string medicalName, string dosage, string frequency, string duration, string instructions, string diagnosis, int doctorId, int patientId)
+        {
+            MedicalName = medicalName;
+            Dosage = dosage;
+            Frequency = frequency;
+            Duration = duration;
+            Instructions = instructions;
+            Diagnosis = diagnosis;
+            DoctorId = doctorId;
+            PatientId = patientId;
+        }
+
+
+    }
+}
---
+++ clinicAPIsSystem/DTOs/PrescriptionDTOs/PrescriptionDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace clinicAPIsSystem.DTOs.PrescriptionDTOs
+{
+    public class PrescriptionDto
+    {
+        public int Id { get; private set; }
+        public string MedicalName { get; private set; }
+        public string Dosage { get; private set; }
+        public string Frequency { get; private set; }
+        public string Duration { get; private set; }
+        public string Instructions { get; private set; }
+        public string Diagnosis { get; private set; }
+        public int DoctorId { get; private set; }
+        public int PatientId { get; private set; }
+
+        public PrescriptionDto(int id, string medicalName, string dosage, string frequency, string duration, string instructions, string diagnosis, int doctorId, int patientId)
+        {
+            Id = id;
+            MedicalName = medicalName;
+            Dosage = dosage;
+            Frequency = frequency;
+            Duration = duration;
+            Instructions = instructions;
+            Diagnosis = diagnosis;
+            DoctorId = doctorId;
+            PatientId = patientId;
+        }
+
+
+    }
+}
