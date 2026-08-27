d83d760e6907efe7a79cd5f53793eb48541c0632
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-28 02:27:11 +0300

    add user repository to use it in service layer

---
+++ clinicAPIsSystem/IRepositories/IUserRepositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using clinicAPIsSystem.Models.User;
+namespace clinicAPIsSystem.IUserRepositories
+{
+    public interface IUserRepository
+    {
+        public Task<ApplicationUser?> GetUserAsync(int id);
+
+        public Task<ApplicationUser?> GetUserByEmailAsync(string email);
+
+        public Task<ApplicationUser?> GetUserByUsernameAsync(string username);
+
+        public Task<bool> UserExistsAsync(int id);
+
+        public Task<bool> EmailExistsAsync(string email);
+
+        public Task<bool> UsernameExistsAsync(string username);
+    }
+}
---
+++ clinicAPIsSystem/Repositories/UserRepositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using clinicAPIsSystem.IUserRepositories;
+using clinicAPIsSystem.Models.User;
+using Microsoft.AspNetCore.Identity;
+namespace clinicAPIsSystem.Repositories.UserRepository
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<ApplicationUser?> GetUserAsync(int id)
+        {
+            return await _userManager.FindByIdAsync(id.ToString());
+        }
+
+        public async Task<ApplicationUser?> GetUserByEmailAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+        public async Task<ApplicationUser?> GetUserByUsernameAsync(string username)
+        {
+            return await _userManager.FindByNameAsync(username);
+        }
+        public async Task<bool> UserExistsAsync(int id)
+        {
+            
+            return (await _userManager.FindByIdAsync(id.ToString())) != null;
+        }
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return (await _userManager.FindByEmailAsync(email)) != null;
+        }
+        public async Task<bool> UsernameExistsAsync(string username)
+        {
+            return (await _userManager.FindByNameAsync(username)) != null;
+        }
+
+    }
+}
