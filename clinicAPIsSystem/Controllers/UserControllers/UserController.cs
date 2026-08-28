a84a7efffddc7f9601cf092b738aebb1daf7b9b6
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-08-28 19:33:09 +0300

    Finish UserController

--- clinicAPIsSystem/Controllers/UserControllers/UserController.cs
+++ clinicAPIsSystem/Controllers/UserControllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using clinicAPIsSystem.DTOs.UserDTOs.ChangePasswordDTO;
 using clinicAPIsSystem.IServices.IUserServices;
+using clinicAPIsSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -15,49 +17,49 @@ namespace clinicAPIsSystem.Controllers
         {
             _userService = userService;
         }
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)},{nameof(UserRole.Receptionist)}")]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _userService.GetUserAsync(id);
             return Ok(user);
         }
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)},{nameof(UserRole.Receptionist)}")]
         [HttpGet("email/{email}")]
         public async Task<IActionResult> GetUserByEmail(string email)
         {
             var user = await _userService.GetUserByEmailAsync(email);
             return Ok(user);
         }
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)},{nameof(UserRole.Receptionist)}")]
         [HttpGet("username/{username}")]
         public async Task<IActionResult> GetUserByUsername(string username)
         {
             var user = await _userService.GetUserByUsernameAsync(username);
             return Ok(user);
         }
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)},{nameof(UserRole.Receptionist)}")]
         [HttpGet("exists/phone/{phoneNumber}")]
         public async Task<IActionResult> PhoneNumberExists(string phoneNumber)
         {
             var result = await _userService.PhoneNumberExistsAsync(phoneNumber);
             return Ok(result);
         }
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)},{nameof(UserRole.Receptionist)}")]
         [HttpGet("exists/email/{email}")]
         public async Task<IActionResult> EmailExists(string email)
         {
             var result = await _userService.EmailExistsAsync(email);
             return Ok(result);
         }
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)},{nameof(UserRole.Receptionist)}")]
         [HttpGet("exists/username/{username}")]
         public async Task<IActionResult> UsernameExists(string username)
         {
             var result = await _userService.UsernameExistsAsync(username);
             return Ok(result);
         }
-
+        [Authorize(Roles = ()]
         [HttpPatch("change-password")]
         public async Task<IActionResult> ChangePassword( [FromBody] ChangePasswordDto request  )
         {
@@ -73,7 +75,7 @@ namespace clinicAPIsSystem.Controllers
             return NoContent();
         }
 
-
+        [Authorize(Roles = $"{nameof(UserRole.Admin)},{nameof(UserRole.Manager)}")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
