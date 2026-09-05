c73d8b47591dc1d43af24699745efa36f2240125
Youssef Ahmed Abdelmageed Ahmed Abdelsalam <yossefahmed6332@gmail.com>
2026-09-05 16:42:18 +0300

    finish logger with cleanerService

--- clinicAPIsSystem/Services/UserServices/EmployeeServices/CleanerService.cs
+++ clinicAPIsSystem/Services/UserServices/EmployeeServices/CleanerService.cs
@@ -12,16 +12,20 @@ namespace clinicAPIsSystem.Services.UserServices.EmployeeServices
         private readonly ICleanerRepository _cleanerRepository; 
         private readonly IUserService _userService; 
         private readonly IMapper _mapper;
+        private readonly ILogger<CleanerService> _logger;
 
-        public CleanerService(ICleanerRepository cleanerRepository, IUserService userService, IMapper mapper)
+        public CleanerService(ICleanerRepository cleanerRepository, IUserService userService, IMapper mapper, ILogger<CleanerService> logger)
         {
             _cleanerRepository = cleanerRepository;
             _userService = userService;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public async Task<CleanerDto> CreateCleanerAsync(CreateCleanerDto createCleanerDto)
         { 
+            _logger
+                .LogInformation("Creating a new cleaner with email: {Email} , username: {UserName} ,salary: {SalaryPerHour} and hours of work {hrsOfWork}", createCleanerDto.Email, createCleanerDto.UserName, createCleanerDto.SalaryPerHour, createCleanerDto.HoursWorked);
             await _userService.ValidateUserCreation(createCleanerDto.Email, createCleanerDto.UserName, createCleanerDto.PhoneNumber);
 
 
@@ -46,25 +50,36 @@ namespace clinicAPIsSystem.Services.UserServices.EmployeeServices
                     await _userService.DeleteUserAsync(
                         createdCleaner.cleaner.Id);
                 }
+                _logger
+                    .LogError("Failed to create cleaner with salary: {SalaryPerHour} and hours of work {hrsOfWork}",  createCleanerDto.SalaryPerHour, createCleanerDto.HoursWorked);
 
                 throw new Exception("Cannot create user, try again");
             }
+            _logger
+                .LogInformation("Successfully created a new cleaner with ID {id} , email: {Email} , username: {UserName} ,salary: {SalaryPerHour} and hours of work {hrsOfWork}",createdCleaner.cleaner.Id, createCleanerDto.Email, createCleanerDto.UserName, createCleanerDto.SalaryPerHour, createCleanerDto.HoursWorked);
 
             return _mapper.Map<CleanerDto>(createdCleaner.cleaner);
         }
 
         public async Task<List<CleanerDto>> GetAllCleanersAsync()
         {
+            _logger
+                .LogDebug("Retrieving all cleaners from the database");
             var cleaners = await _cleanerRepository.GetAllCleanersAsync();
             return _mapper.Map<List<CleanerDto>>(cleaners);
         }
 
         public async Task<CleanerDto> GetCleanerAsync(int cleanerId)
         {
+            _logger
+                .LogDebug("Retrieving cleaner with ID {id} from the database", cleanerId);
             var cleaner = await _cleanerRepository.GetCleanerAsync(cleanerId);
             if (cleaner == null)
             {
+                _logger
+                .LogWarning("Cleaner with ID {id} not found in the database", cleanerId);
                 throw new Exception("Cleaner not found");
+
             }
             return _mapper.Map<CleanerDto>(cleaner);
         }
@@ -73,15 +88,21 @@ namespace clinicAPIsSystem.Services.UserServices.EmployeeServices
             UpdateCleanerDto updateCleanerDto,
             int id)
         {
+            _logger
+                .LogInformation("Updating cleaner with ID {id} ", id);
             await _userService.ValidateUserCreation(
                 updateCleanerDto.Email,
                 updateCleanerDto.UserName,
                 updateCleanerDto.PhoneNumber);
 
             var cleaner = await _cleanerRepository.GetCleanerAsync(id);
+            _logger
+                .LogDebug("Retrieving cleaner with ID {id} from the database", id);
 
             if (cleaner == null)
             {
+                _logger
+                    .LogWarning("Cleaner with ID {id} not found in the database", id);
                 throw new KeyNotFoundException(
                     $"Cannot find cleaner with id {id}");
             }
@@ -100,6 +121,8 @@ namespace clinicAPIsSystem.Services.UserServices.EmployeeServices
 
             var updatedCleaner =
                 await _cleanerRepository.UpdateCleanerAsync(cleaner);
+            _logger
+                .LogInformation("Successfully updated cleaner with ID {id} ", id);
 
             return _mapper.Map<CleanerDto>(updatedCleaner);
         }
